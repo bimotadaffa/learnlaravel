a23cb3dcbf92a05fedab50d669e9c051b4c6241a
bimotadaffa <bimotacollage@gmail.com>
2024-10-21 12:36:00 +0700

    Deskripsi perubahan

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
-  <x-slot:title>{{ $title }}></x-slot:title>
+  <x-slot:title>{{ $title }}</x-slot:title>
   <h3 class="test-xl">About</h3>
 </x-layout>
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
-  <x-slot:title>{{ $title }}></x-slot:title>
+  <x-slot:title>{{ $title }}</x-slot:title>
   <h3 class="test-xl">Welcome to my blog</h3>
 </x-layout>
