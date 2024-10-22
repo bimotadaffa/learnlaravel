50ee772f90d540d7760311172da5ecb9d480f2e9
bimotadaffa <bimotacollage@gmail.com>
2024-10-22 16:17:44 +0700

    blog

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,13 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
+            'id' => 1,
             'title' => 'Judul Artikel 1',
             'author' => 'Bimota Ares',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, quibusdam nostrum odio est facere ex fugiat magni? Ab harum aut esse ex facilis ipsum, voluptas error! Laboriosam porro nostrum dicta.'
         ],
         [
+            'id' => 2,
             'title' => 'Judul Artikel 2',
             'author' => 'Bimota Ares',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa quasi ratione saepe consequatur odio eos, soluta aliquam. Dolorum voluptas ex vel totam rem repellendus iusto numquam quisquam, provident deserunt! Blanditiis.'
