b9ddd7e69078ad59ad05b0b0c58389ef6c6c9e29
bimotadaffa <bimotacollage@gmail.com>
2024-10-23 19:00:31 +0700

    navbar

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,14 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Bimota Ares',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, quibusdam nostrum odio est facere ex fugiat magni? Ab harum aut esse ex facilis ipsum, voluptas error! Laboriosam porro nostrum dicta.'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Bimota Ares',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa quasi ratione saepe consequatur odio eos, soluta aliquam. Dolorum voluptas ex vel totam rem repellendus iusto numquam quisquam, provident deserunt! Blanditiis.'
@@ -27,24 +29,26 @@ Route::get('/posts', function () {
     ]]);
 });
 
-Route::get('/posts/{id}', function($id){
+Route::get('/posts/{slug}', function($slug){
     $posts = [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Bimota Ares',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, quibusdam nostrum odio est facere ex fugiat magni? Ab harum aut esse ex facilis ipsum, voluptas error! Laboriosam porro nostrum dicta.'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Bimota Ares',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa quasi ratione saepe consequatur odio eos, soluta aliquam. Dolorum voluptas ex vel totam rem repellendus iusto numquam quisquam, provident deserunt! Blanditiis.'
         ]
         ];
 
-    $post = Arr::first($posts, function($post) use ($id) {
-        return $post['id'] == $id;
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
     });
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
