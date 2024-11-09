a485a4422a56190960650b82f927229dafe0b7d1
bimotadaffa <bimotacollage@gmail.com>
2024-11-09 17:55:33 +0700

    pagination

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(5)]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post){
